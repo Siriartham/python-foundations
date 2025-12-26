98bc14d1a392d9bd2e9b50d410c432f4667486b8
Artham Siri <siriartham83@gmail.com>
2025-12-26 08:34:19 +0530

    Initialize variables and print their values examples
    Added variable declarations and print statements in basics.py.

---
+++ basics.py
@@ -0,0 +1,12 @@
+name = "Johan"    # Here we are declaring a variable , of String datatype.
+age = 20          # Here we are declaring a variable , of Integer datatype.
+height = 5.4      # Here we are declaring a variable , of Float datatype.
+is_student = True # Here we are declaring a variable , of Boolean datatype.
+
+# Variables are the containers used to store the value in the memory to reuse the data
+#In the above name,age,height,is_student are the variables and Johan,20,5.4,True are the values.
+
+print(name)       # Here we are printing the value of the variable for such cases print function is used.
+print(age)
+print(height)
+print(is_student)
